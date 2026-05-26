06064481ffa3008a1a8d938fd42e8b9bb5d6221b
Anshika2194 <srianshika2194@gmail.com>
2026-05-26 22:24:18 +0530

    added Generic Quantity Class with Unit Interface for Multi-Category Support

---
+++ src/main/java/org/apps/QuantityMeasurement/IMeasurable.java
@@ -0,0 +1,11 @@
+package org.apps.QuantityMeasurement;
+
+public interface IMeasurable {
+    double getConversionFactor();
+
+    double convertToBaseUnit(double value);
+
+    double convertFromBaseUnit(double baseValue);
+
+    String getUnitName();
+}
